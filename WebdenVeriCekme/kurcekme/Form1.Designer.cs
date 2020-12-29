8a06c9a97c00334ecfad0ce7f5e7c843cc549d71
Samed Bilgili <s.bilgili@ulogi.com>
2020-12-29 11:36:11 +0300

    first commit

---
+++ WebdenVeriCekme/kurcekme/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace kurcekme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUsd = new System.Windows.Forms.TextBox();
+            this.btnGetData = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEuro = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnHepsiburadaFiyatGetir = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblMesaj = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtUsd
+            // 
+            this.txtUsd.Location = new System.Drawing.Point(164, 45);
+            this.txtUsd.Name = "txtUsd";
+            this.txtUsd.Size = new System.Drawing.Size(100, 20);
+            this.txtUsd.TabIndex = 0;
+            // 
+            // btnGetData
+            // 
+            this.btnGetData.Location = new System.Drawing.Point(164, 83);
+            this.btnGetData.Name = "btnGetData";
+            this.btnGetData.Size = new System.Drawing.Size(206, 23);
+            this.btnGetData.TabIndex = 1;
+            this.btnGetData.Text = "Verileri Getir";
+            this.btnGetData.UseVisualStyleBackColor = true;
+            this.btnGetData.Click += new System.EventHandler(this.btnGetData_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(164, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "USD";
+            // 
+            // txtEuro
+            // 
+            this.txtEuro.Location = new System.Drawing.Point(270, 45);
+            this.txtEuro.Name = "txtEuro";
+            this.txtEuro.Size = new System.Drawing.Size(100, 20);
+            this.txtEuro.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(270, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "EURO";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(58, 199);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(206, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Trendyol - İphone 12";
+            // 
+            // btnHepsiburadaFiyatGetir
+            // 
+            this.btnHepsiburadaFiyatGetir.Location = new System.Drawing.Point(164, 261);
+            this.btnHepsiburadaFiyatGetir.Name = "btnHepsiburadaFiyatGetir";
+            this.btnHepsiburadaFiyatGetir.Size = new System.Drawing.Size(206, 23);
+            this.btnHepsiburadaFiyatGetir.TabIndex = 5;
+            this.btnHepsiburadaFiyatGetir.Text = "Verileri Getir";
+            this.btnHepsiburadaFiyatGetir.UseVisualStyleBackColor = true;
+            this.btnHepsiburadaFiyatGetir.Click += new System.EventHandler(this.btnHepsiburadaFiyatGetir_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(285, 199);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(206, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(282, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(124, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Hepsiburada - İphone 12";
+            // 
+            // lblMesaj
+            // 
+            this.lblMesaj.AutoSize = true;
+            this.lblMesaj.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblMesaj.Location = new System.Drawing.Point(206, 231);
+            this.lblMesaj.Name = "lblMesaj";
+            this.lblMesaj.Size = new System.Drawing.Size(66, 20);
+            this.lblMesaj.TabIndex = 6;
+            this.lblMesaj.Text = "lblMesaj";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 351);
+            this.Controls.Add(this.lblMesaj);
+            this.Controls.Add(this.btnHepsiburadaFiyatGetir);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGetData);
+            this.Controls.Add(this.txtEuro);
+            this.Controls.Add(this.txtUsd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUsd;
+        private System.Windows.Forms.Button btnGetData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEuro;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnHepsiburadaFiyatGetir;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblMesaj;
+    }
+}
+
---
+++ WebdenVeriCekme/kurcekme/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace kurcekme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetData_Click(object sender, EventArgs e)
+        {
+            string exchangeRate = "http://www.tcmb.gov.tr/kurlar/today.xml";
+            var xmlDoc = new XmlDocument();
+            xmlDoc.Load(exchangeRate);
+
+            string usd = xmlDoc.SelectSingleNode("Tarih_Date/Currency[@Kod='USD']/BanknoteSelling").InnerXml;
+            string euro = xmlDoc.SelectSingleNode("Tarih_Date/Currency[@Kod='EUR']/BanknoteSelling").InnerXml;
+
+            txtUsd.Text = usd;
+            txtEuro.Text = euro;
+        }
+
+        public string clearPriceTag(string priceTag)
+        {
+            priceTag = priceTag.Replace(" ", "");
+            priceTag = priceTag.Replace("TL", "");
+
+            return priceTag;
+        }
+
+        private void btnHepsiburadaFiyatGetir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                trendyolFiyatGetir();
+                hepsiburadaFiyatGetir();
+
+                decimal trendyol = Convert.ToDecimal(clearPriceTag(textBox1.Text));
+                decimal hepsiburada = Convert.ToDecimal(clearPriceTag(textBox2.Text));
+
+                if (trendyol < hepsiburada)
+                {
+                    lblMesaj.Text = "Trenyol " + (hepsiburada - trendyol) + " TL daha kazançlı";
+                }
+                else
+                {
+                    lblMesaj.Text = "Hepsiburada " + (trendyol - hepsiburada) + " TL daha kazançlı";
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void trendyolFiyatGetir()
+        {
+            var url = new Uri("https://www.trendyol.com/apple/iphone-12-128gb-mavi-cep-telefonu-apple-turkiye-garantili-sarj-aleti-ve-kulaklik-harictir-p-66403057?boutiqueId=547744&merchantId=104872"); // url oluştruduk
+            var client = new WebClient(); // siteye erişim için client tanımladık
+
+            client.Headers.Add("User-Agent: Other");
+            var html = client.DownloadString(url); //sitenin html lini indirdik
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument(); //burada HtmlAgilityPack Kütüphanesini kullandık
+            doc.LoadHtml(html); // indirdiğimiz sitenin html lini oluşturduğumuz dokumana dolduruyoruz
+            var veri = doc.DocumentNode.SelectNodes("//*[@id='product-detail-app']/div/div[2]/div[2]/div[1]/div[1]/div[1]/div[3]/div/div/span")[0]; // siteden aldığımız xpath i buraya yazıp kaynak kısmını seçiyoruz
+            if (veri != null)
+            {
+                textBox1.Text = veri.InnerHtml;
+            }
+        }
+
+        public void hepsiburadaFiyatGetir()
+        {
+            var url = new Uri("https://www.hepsiburada.com/iphone-12-64-gb-p-HBV00000YDZXF"); // url oluştruduk
+            var client = new WebClient(); // siteye erişim için client tanımladık
+            client.Headers.Add("Accept: text/html, application/xhtml+xml, */*");
+            client.Headers.Add("User-Agent: Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)");
+            var html = client.DownloadString(url); //sitenin html lini indirdik
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument(); //burada HtmlAgilityPack Kütüphanesini kullandık
+            doc.LoadHtml(html); // indirdiğimiz sitenin html lini oluşturduğumuz dokumana dolduruyoruz
+            var veri = doc.DocumentNode.SelectNodes("//*[@id='offering-price']/span[1]")[0]; // siteden aldığımız xpath i buraya yazıp kaynak kısmını seçiyoruz
+            if (veri != null)
+            {
+                textBox2.Text = veri.InnerHtml;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblMesaj.Text = "";
+        }
+    }
+}
